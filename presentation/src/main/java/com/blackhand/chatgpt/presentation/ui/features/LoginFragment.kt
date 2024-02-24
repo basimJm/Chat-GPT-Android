f8685b793581bb08c51fe11b1fd0f59b9cb8c7ef
basim jawad <basemjm998@gmail.com>
2024-02-24 18:31:45 +0300

    create signup fragment
    create signup fragment in logActivity
    
     closes #2

--- presentation/src/main/java/com/blackhand/chatgpt/presentation/ui/features/LoginFragment.kt
+++ presentation/src/main/java/com/blackhand/chatgpt/presentation/ui/features/LoginFragment.kt
@@ -1,10 +1,12 @@
-package com.blackhand.chatgpt.presentation.ui
+package com.blackhand.chatgpt.presentation.ui.features
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.blackhand.chatgpt.presentation.R
 import com.blackhand.chatgpt.presentation.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -14,8 +16,19 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.btnSignUp.setOnClickListener {
+            findNavController().navigate(R.id.signUpFragment)
+        }
     }
 }
